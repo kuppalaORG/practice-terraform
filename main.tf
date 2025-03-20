5a4a9d91a60343283487ef0c4c17e9add31e056e
bharathkuppala <bharath.kuppala@simplifyvms.com>
2025-03-20 14:26:21 +0530

    added provisioners

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "null_resource" "ansible" {
 
     inline = [
       "sudo pip-3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/raghudevopsb80/roboshop-ansible main.yml -e env=dev -e role_name=${each.key}"
+      "ansible-pull -i localhost, -U https://github.com/kuppalaORG/basic_ansible main.yml -e env=dev -e role_name=${each.key}"
     ]
 
   }
